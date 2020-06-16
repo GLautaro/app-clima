bcf94e18eb7c5cbaccd2812a6d5c7956bbb58270
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2020-06-16 14:56:08 -0300

    Clase 3
    Se añaden constantes, prop-types y estilos CSS a los componentes.
    Se refactoriza el proyecto utilizando carpetas para manejar los componentes

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
+import { CLOUD, SUN, CLOUDY } from '../../../constants/weathers';
+import './styles.css';
 
 const WeatherData = () => {
     return(
-        <div>
-            <WeatherTemperature temperature={20} weatherState={"fog"}/>
+        <div className='weatherDataCont'>
+            <WeatherTemperature temperature={30} weatherState={SUN}/>
             <WeatherExtraInfo humidity={70} wind={"5 m/s"}/>
         </div>
     );
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import './styles.css';
 
 const WeatherLocation = () => {
     //return de la estructura del componente
     return(
-    <div>
+    <div className='weatherLocationCont'>
         <Location city='Córdoba'/>
         <WeatherData/>
     </div>
