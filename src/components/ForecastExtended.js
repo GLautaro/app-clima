e99dcc9a01ba969aae372539868da840d2b0e8b3
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2020-07-16 13:31:09 -0300

    Clase 11
    Utilizamos combine reducers, modificamos forecast extended para que sea functional component y utilizamos selectores.

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,77 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
-import transformForecast from '../services/transformForecast';
-import getUrlForecastCity from '../services/getUrlForecastCity';
 import './styles.css';
 
+const renderForecastItemDays = (forecastData) => {
+    return forecastData.map( forecast => <ForecastItem 
+        key={`${forecast.weekDay}${forecast.hour}`} 
+        weekDay={forecast.weekDay} 
+        hour={forecast.hour} 
+        data={forecast.data} /> );
+};
 
-class ForecastExtended extends Component {
+const renderProgress = () => {
+    return <h3>Cargando pronostico extendido...</h3>
+};
+ 
 
-    constructor() {
-        super();
-        this.state = {
-            forecastData: null,
-        }
-    }
-
-    componentDidMount() {
-        this.updateCity(this.props.city);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.city !== this.props.city) {
-            this.setState({ forecastData: null })
-            this.updateCity(nextProps.city);
-        }
-    }
-
-
-    updateCity = city => {
-        //fetch o axios
-        const url = getUrlForecastCity(city);
-        fetch(url).then(
-            data => (data.json())
-        ).then(
-            weather_data => {
-                console.log(weather_data);
-                const forecastData = transformForecast(weather_data);
-                console.log(forecastData);
-                this.setState({
-                    forecastData,
-                })
-            }
-        )
-    }
-
-    renderForecastItemDays( forecastData ) {
-        return forecastData.map( forecast => <ForecastItem 
-            key={`${forecast.weekDay}${forecast.hour}`} 
-            weekDay={forecast.weekDay} 
-            hour={forecast.hour}
-            data={forecast.data}/>)
-    }
-
-    renderProgress() {
-        return <h3>Cargando pronostico extendido...</h3>
-    }
-
-    render() {
-        const { forecastData } = this.state;
-        const { city } = this.props
-        return (
-            <div>
-                <h2 className="forecast-title"> Pronostico extendido para {city} </h2>
-                { forecastData ? 
-                    this.renderForecastItemDays(forecastData) : 
-                    this.renderProgress()}
-            </div>
-        );
-    }
-}
+const ForecastExtended = ({ city, forecastData }) => (
+    <div>
+        <h2 className="forecast-title">
+            Pronostico Extendido para {city}
+        </h2>
+        { forecastData ? 
+        renderForecastItemDays(forecastData) :
+        renderProgress()}
+    </div>
+)
+        
 
 ForecastExtended.propTypes = {
     city: PropTypes.string.isRequired,
+    forecastData: PropTypes.array,
 }
 
 export default ForecastExtended;
